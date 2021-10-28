baadf93d748805319c60c46d026af789d8ccade1
SlumberDemon <72651859+SlumberDemon@users.noreply.github.com>
2021-10-28 13:54:54 +0200

    added /

--- src/main.py
+++ src/main.py
@@ -13,11 +13,20 @@ intents.members = True
 
 bot = commands.Bot(command_prefix='-', intents=intents)
 inter_client = InteractionClient(bot, modify_send=False) # modify_send=False to make discord.py 2.0 views work
+guilds = [877399405056102431, 819112190115446844]
 
 # Stuff
 
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+
+@inter_client.slash_command(
+    name="hello", # Defaults to the function name
+    description="Says hello",
+    guild_ids=guilds
+)
+async def hello(inter):
+    await inter.reply("Hello!")
 
 bot.run(getenv('TOKEN'))
