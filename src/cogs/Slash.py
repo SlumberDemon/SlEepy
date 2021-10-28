d5e433148e45ec75a85e5eb4e48271f52cb7f260
SlumberDemon <72651859+SlumberDemon@users.noreply.github.com>
2021-10-28 14:51:56 +0200

    added )

--- src/cogs/Slash.py
+++ src/cogs/Slash.py
@@ -8,6 +8,8 @@ class Slash(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    # Utility
+
     @slash_command(description='Shows bot latency')
     async def latency(self, ctx):
         embed = discord.Embed(description=f'🏓 Pong! {round(self.bot.latency * 1000)}ms', colour=0xc3d9df)
@@ -16,7 +18,7 @@ class Slash(commands.Cog):
     @slash_command(description='Bot invite')
     async def invite(self, ctx):
         embed = discord.Embed(description='[invite](https://discord.com/api/oauth2/authorize?client_id=903187756254130177&permissions=3557156934&scope=applications.commands%20bot)', colour=0xc3d9df)
-        await ctx.send(embed=embed
+        await ctx.send(embed=embed)
 
     @slash_command(description='Shows info on the developer')
     async def slumberdemon(self, ctx):
@@ -24,6 +26,12 @@ class Slash(commands.Cog):
         embed.set_thumbnail(url='https://cdn.discordapp.com/avatars/549563196579774465/f1df0e07a490bfbeb4704a54d6181fd8.webp?size=1024')
         embed.add_field(name='Website', value='[click here](https://slumberdemon.carrd.co/)')
         await ctx.send(embed=embed)
+    
+    # Mod
+
+
+
+
 
 def setup(bot):
     bot.add_cog(Slash(bot))
