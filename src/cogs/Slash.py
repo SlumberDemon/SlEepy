fe2566ad1d98fc47c7575a31164d8bfade447ad5
SlumberDemon <72651859+SlumberDemon@users.noreply.github.com>
2021-10-28 14:40:36 +0200

    added colour to /invite

--- src/cogs/Slash.py
+++ src/cogs/Slash.py
@@ -15,7 +15,7 @@ class Slash(commands.Cog):
 
     @slash_command(description='Bot invite')
     async def invite(self, ctx):
-        embed = discord.Embed(description='[invite](https://discord.com/api/oauth2/authorize?client_id=903187756254130177&permissions=3557156934&scope=applications.commands%20bot)')
+        embed = discord.Embed(description='[invite](https://discord.com/api/oauth2/authorize?client_id=903187756254130177&permissions=3557156934&scope=applications.commands%20bot)', colour=0xc3d9df)
         await ctx.send(embed=embed, ephemeral=True)
 
     @slash_command(description='Shows info on the developer')
