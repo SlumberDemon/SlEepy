d9b0e3aa4872b7003d4d35d75c96d9b647c4c3ed
SlumberDemon <72651859+SlumberDemon@users.noreply.github.com>
2021-10-29 14:54:30 +0200

    fix and change

--- src/main.py
+++ src/main.py
@@ -12,15 +12,7 @@ intents.members = True
 # Setup
 
 bot = commands.Bot(command_prefix='-', intents=intents)
-inter_client = InteractionClient(bot, modify_send=False) # modify_send=False to make discord.py 2.0 views work
-guilds = [877399405056102431, 819112190115446844]
-
-# Stuff
-
-@bot.command(aliases=['ping'], help='Shows bot latency')
-async def latency(ctx):
-    embed = discord.Embed(description=f'🏓 Pong! {round(bot.latency * 1000)}ms', colour=0xc3d9df)
-    await ctx.send(embed=embed)
+inter_client = InteractionClient(bot, modify_send=False, help=None) # modify_send=False to make discord.py 2.0 views work
 
 # Cogs
 
