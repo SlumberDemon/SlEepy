4fddb35c5d3ed78dba879946f4f9b63b2412e71e
SlumberDemon <72651859+SlumberDemon@users.noreply.github.com>
2021-10-30 10:52:21 +0200

    import fix

--- src/cogs/Error.py
+++ src/cogs/Error.py
@@ -1,6 +1,5 @@
 import discord, dislash
 from discord.ext import commands
-from src.extras.emojis import Emo
 
 class Listener(commands.Cog):
 
@@ -12,7 +11,7 @@ class Listener(commands.Cog):
     async def on_slash_command_error(self, inter, error):
         channel = self.bot.get_channel(903926683185348619)
         await channel.send(error)
-        
+
 
 def setup(bot):
     bot.add_cog(Listener(bot))
