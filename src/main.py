de794c6dcd6435729b783e156a02dd2677f0865e
SlumberDemon <72651859+SlumberDemon@users.noreply.github.com>
2021-11-03 10:15:24 +0100

    removed help command in the main.py file

--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ intents.members = True
 
 # Setup
 
-bot = commands.Bot(command_prefix='-', intents=intents)
+bot = commands.Bot(command_prefix='-', intents=intents, help_command=None)
 inter_client = InteractionClient(bot, modify_send=False) # modify_send=False to make discord.py 2.0 views work
 
 # Cogs
