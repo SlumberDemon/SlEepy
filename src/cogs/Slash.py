33cc58e5256a6fdc185523a53fea4c870733bacc
SlumberDemon <72651859+SlumberDemon@users.noreply.github.com>
2021-11-08 14:46:35 +0100

    added /edit to edit bot messages

--- src/cogs/Slash.py
+++ src/cogs/Slash.py
@@ -133,5 +133,15 @@ class Slash(commands.Cog):
         await msg.edit('🎉 **GIVEAWAY ENDED** 🎉', embed=embed2)
         await ctx.send(f'Congratulations {winner.mention}! You won the **{prize}**!', view=view)
 
+    @dislash.slash_command(description='Edit commands')
+    async def edit(self, inter: SlashInteraction):
+        pass
+
+    @edit.sub_command(description='Edit bot messages')
+    async def message(self, ctx, message_id, channel: channel: discord.TextChannel, *, message):
+        msg = await channel.fetch_message(message_id)
+        await msg.edit(content=message)
+
+
 def setup(bot):
     bot.add_cog(Slash(bot))
