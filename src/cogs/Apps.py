a29553d4768e8d1d2434ddacec2dd81814ba5829
SlumberDemon <72651859+SlumberDemon@users.noreply.github.com>
2021-11-25 17:44:31 +0100

    self

--- src/cogs/Apps.py
+++ src/cogs/Apps.py
@@ -8,7 +8,7 @@ class App(commands.Cog):
         self.bot = bot
 
     @dislash.user_command(name="Created at")
-    async def created_at(inter: ContextMenuInteraction):
+    async def created_at(self, inter: ContextMenuInteraction):
         # User commands always have only this ^ argument
         await inter.respond(
             f"{inter.user} was created at {inter.user.created_at}",
