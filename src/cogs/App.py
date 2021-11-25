7fa4cbc50fe716ec96ad690053b94e2981ef2007
SlumberDemon <72651859+SlumberDemon@users.noreply.github.com>
2021-11-25 17:34:50 +0100

    apps

---
+++ src/cogs/App.py
@@ -0,0 +1,19 @@
+import discord, dislash
+from dislash import slash_command, SlashInteraction
+from discord.ext import commands
+
+class App(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @dislash.user_command(name="Created at")
+    async def created_at(inter: ContextMenuInteraction):
+        # User commands always have only this ^ argument
+        await inter.respond(
+            f"{inter.user} was created at {inter.user.created_at}",
+            ephemeral=True # Make the message visible only to the author
+        )
+
+def setup(bot):
+    bot.add_cog(App(bot))
