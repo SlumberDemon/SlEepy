8bcd1f204f4fcba919af1b75c4284ae4cc88b08e
SlumberDemon <72651859+SlumberDemon@users.noreply.github.com>
2021-11-25 18:30:22 +0100

    quote

--- src/cogs/Apps.py
+++ src/cogs/Apps.py
@@ -9,21 +9,22 @@ class Apps(commands.Cog):
 
     @dislash.user_command(name="Created at")
     async def created_at(self, inter: ContextMenuInteraction):
-        # User commands always have only this ^ argument
         await inter.respond(
             f"{inter.user} was created at {inter.user.created_at}",
-            ephemeral=True # Make the message visible only to the author
+            ephemeral=True 
         )
 
     @dislash.message_command(name="Reverse")
     async def reverse(self, inter: ContextMenuInteraction):
-        # Message commands always have only this ^ argument
         if inter.message.content:
-            # Here we will send a reversed message to the chat
             await inter.respond(inter.message.content[::-1])
         else:
-            # Here we will explain that the message isn't valid
             await inter.respond("There's no content", ephemeral=True)
+
+    @dislash.message_command(name="Quote")
+    async def quote(self, ctx: ContextMenuInteraction):
+        embed = discord.Embed(description=inter.message.content)
+        await ctx.respond(embed=embed)
 
 def setup(bot):
     bot.add_cog(Apps(bot))
