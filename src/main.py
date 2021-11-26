94b63ae068c79fdf761b8adb3d975d29bccc2ec3
SlumberDemon <72651859+SlumberDemon@users.noreply.github.com>
2021-11-26 08:21:28 +0100

    test cmd

--- src/main.py
+++ src/main.py
@@ -14,6 +14,15 @@ intents.members = True
 bot = commands.Bot(command_prefix='-', intents=intents, help_command=None)
 inter_client = InteractionClient(bot, modify_send=False) # modify_send=False to make discord.py 2.0 views work
 
+@bot.command()
+@commands.is_owner()
+async def gl(ctx):
+    for guild in bot.guilds:
+        prefix = await prefix_fetcher(guild.id)
+        autorole = await autorole_fetcher(guild.id)
+        embed = discord.Embed(title=f'Guild-{guild.id}', description=f'` Name ` {guild} \n ` Members ` {guild.member_count} \n ` Prefix ` {prefix} \n ` Autorole ` {autorole}') 
+        await ctx.send(embed=embed)
+
 # Cogs
 
 bot.load_extension('cogs.Slash')
